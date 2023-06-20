bf93264568fbdf7ef799054c7e5fe1caa1a40365
Tamok <nautiluce@gmail.com>
2023-06-19 18:48:44 -0700

    Fix import path for rootReducer in index.js

--- DoToWin/src/store/index.js
+++ DoToWin/src/store/index.js
@@ -9,11 +9,6 @@ import { persistStore, persistReducer } from 'redux-persist';
 import storage from 'redux-persist/lib/storage'; // defaults to localStorage for web
 
 // Import reducers
-import tasksReducer from './tasksSlice';
-import gameStatsReducer from './gameStatsSlice';
-import characterReducer from './characterSlice';
-import townReducer from './townSlice';
-
 import rootReducer from './reducers';
 
 // Configuration for redux persist
